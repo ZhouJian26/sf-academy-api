58ebc172fdf1654c2eaabffe6ac0f1b292dfcf85
Zhou Jian <zhoujian0998@gmail.com>
2020-07-25 14:52:43 +0200

    added exchange api definition

---
+++ api/v1/paths/exchange/exchange.js
@@ -0,0 +1,40 @@
+"use strict";
+const Exchange = (exchangeMicroservice) => {
+  const operations = { GET };
+  function GET(req, res, next) {
+    const { value, from, to } = req.query;
+    exchangeMicroservice
+      .getExchange(value, from, to)
+      .then((data) => res.status(200).json(data))
+      .catch((err) => res.status(400).json({ message: err.details }));
+  }
+  GET.apiDoc = {
+    tags: ["exchange"],
+    description: "Exchange the sum of money in another currency",
+    summary: "Exchange the sum of money in another currency",
+    operationId: "getExchange",
+    produces: ["application/json"],
+    responses: {
+      200: {
+        description: "Successful operation",
+        schema: {
+          type: "object",
+          properties: {
+            value: {
+              type: "number",
+              description: "Converted value",
+            },
+          },
+        },
+      },
+      default: {
+        description: "An error occurred",
+        schema: {
+          $ref: "#definitions/Error",
+        },
+      },
+    },
+  };
+  return operations;
+};
+module.exports = Exchange;
