e98c4dc81504b4b17ea7b0aef757f7850d86c079
Zhou Jian <zhoujian0998@gmail.com>
2020-07-31 11:24:39 +0200

    deposit openapi docs

---
+++ src/api/v1/paths/user/deposit.js
@@ -0,0 +1,56 @@
+const Deposit = (userMicroservice) => {
+  const operations = { PUT };
+  function PUT(req, res, next) {
+    res.json({ message: "hello there" });
+  }
+  PUT.apiDoc = {
+    tags: ["user"],
+    description:
+      "Deposit an amount of a target currency into the virtual portfolio",
+    summary:
+      "Deposit an amount of a target currency into the virtual portfolio",
+    operationId: "userDeposit",
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    parameters: [
+      {
+        in: "body",
+        name: "body",
+        required: true,
+        schema: {
+          type: "object",
+          properties: {
+            amount: {
+              type: "number",
+              minimum: 0,
+              multipleOf: 0.01,
+            },
+            currency: { type: "string" },
+          },
+          required: ["amount", "currency"],
+        },
+      },
+    ],
+    responses: {
+      200: {
+        description: "Successful operation",
+        schema: {
+          type: "object",
+          properties: {
+            message: {
+              type: "string",
+            },
+          },
+        },
+      },
+      default: {
+        description: "An error occurred",
+        schema: {
+          $ref: "#definitions/Error",
+        },
+      },
+    },
+  };
+  return operations;
+};
+module.exports = Deposit;
