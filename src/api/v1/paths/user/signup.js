1a1421f63a2dbc55eb30c5df67e17310c523b16f
Zhou Jian <zhoujian0998@gmail.com>
2020-07-31 11:25:35 +0200

    signup openapi docs

---
+++ src/api/v1/paths/user/signup.js
@@ -0,0 +1,52 @@
+const Signup = (userMicroservice) => {
+  const operations = { POST };
+  function POST(req, res, next) {
+    res.json({ message: "hello there" });
+  }
+  POST.apiDoc = {
+    tags: ["user"],
+    description: "Singup a new user",
+    summary: "Singup a new user",
+    operationId: "userSignup",
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    parameters: [
+      {
+        in: "body",
+        name: "body",
+        required: true,
+        schema: {
+          type: "object",
+          properties: {
+            email: { type: "string", format: "email" },
+            password: { type: "string", format: "password" },
+            username: { type: "string" },
+            iban: { type: "string" },
+          },
+          required: ["email", "password", "username", "iban"],
+        },
+      },
+    ],
+    responses: {
+      200: {
+        description: "Successful operation",
+        schema: {
+          type: "object",
+          properties: {
+            message: {
+              type: "string",
+            },
+          },
+        },
+      },
+      default: {
+        description: "An error occurred",
+        schema: {
+          $ref: "#definitions/Error",
+        },
+      },
+    },
+  };
+  return operations;
+};
+module.exports = Signup;
