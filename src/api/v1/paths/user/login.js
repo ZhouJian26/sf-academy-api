50c618a455ac5ad3995dc4d55a34b7ff90eb3aa7
Zhou Jian <zhoujian0998@gmail.com>
2020-07-31 11:25:23 +0200

    login openapi docs

---
+++ src/api/v1/paths/user/login.js
@@ -0,0 +1,50 @@
+const Login = (userMicroservice) => {
+  const operations = { POST };
+  function POST(req, res, next) {
+    res.json({ message: "hello there" });
+  }
+  POST.apiDoc = {
+    tags: ["user"],
+    description: "Login a user",
+    summary: "Login a user",
+    operationId: "userLogin",
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    parameters: [
+      {
+        in: "body",
+        name: "body",
+        required: true,
+        schema: {
+          type: "object",
+          properties: {
+            email: { type: "string", format: "email" },
+            password: { type: "string", format: "password" },
+          },
+          required: ["email", "password"],
+        },
+      },
+    ],
+    responses: {
+      200: {
+        description: "Successful operation",
+        schema: {
+          type: "object",
+          properties: {
+            message: {
+              type: "string",
+            },
+          },
+        },
+      },
+      default: {
+        description: "An error occurred",
+        schema: {
+          $ref: "#definitions/Error",
+        },
+      },
+    },
+  };
+  return operations;
+};
+module.exports = Login;
