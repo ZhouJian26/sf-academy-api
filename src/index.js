49b5227c97c4e7cae997f70e8a7902e0b96cd690
Zhou Jian <zhoujian0998@gmail.com>
2020-08-01 11:09:43 +0200

    add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 "use strict";
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const { initialize } = require("express-openapi");
 const { join } = require("path");
 
@@ -10,6 +11,7 @@ const exchangeMicroservice = require("./services/exchange");
 const userMicroservice = require("./services/user");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const securityHandlers = {
